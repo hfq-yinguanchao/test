faf7cd1d361d093561298cbfdc8f4a66254f338b
yinguanchao <752356771@qq.com>
2018-05-18 12:11:51 +0800

    22

--- src/test/Student.java
+++ src/test/Student.java
@@ -2,9 +2,18 @@ package test;
 
 public class Student {
 	private Integer pageNum;
+	private Integer pageNum22;
 
 	public Integer getPageNum() {
 		return pageNum;
+	}
+
+	public Integer getPageNum22() {
+		return pageNum22;
+	}
+
+	public void setPageNum22(Integer pageNum22) {
+		this.pageNum22 = pageNum22;
 	}
 
 	public void setPageNum(Integer pageNum) {
